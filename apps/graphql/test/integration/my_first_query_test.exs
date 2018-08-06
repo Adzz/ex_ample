106835655e1cd1e49ba5d61a9a1696b9a1a35d75
Adam Lancaster <lancaster28@hotmail.com>
2018-08-06 16:09:25 +0100

    Add first failing integration test for query

---
+++ apps/graphql/test/integration/my_first_query_test.exs
@@ -0,0 +1,9 @@
+defmodule MyFirstQueryTest do
+  use GraphqlWeb.ConnCase
+
+  describe "queries for the field successfully" do
+    test "Queries for Average STC (in days) successfully" do
+      # seed database
+    end
+  end
+end
