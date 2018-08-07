faded1f3955fa6ac9df50843fb5976fe3a8c46e0
Adam Lancaster <lancaster28@hotmail.com>
2018-08-07 12:11:34 +0100

    Add create for DB

--- apps/ex_ample_backend/lib/land_reg_data.ex
+++ apps/ex_ample_backend/lib/land_reg_data.ex
@@ -17,9 +17,12 @@ defmodule LandRegData do
     sale_price: :number,
     postcode: :string,
     house_number: :number,
-    date_sold: :date
+    date_sold: :date,
+    average_time_to_sold: :number
   }
   @column_set MapSet.new(Map.keys(@columns))
+  def column_set, do: @column_set
+
   defstruct Map.keys(@columns) ++ [module: __MODULE__]
 
   ## Client API
@@ -44,7 +47,12 @@ defmodule LandRegData do
   Yells if you you try and update attribues that the record doesn't have.
   """
   def update(server, record, changes) do
-    GenServer.cast(server, {:update, record, changes})
+    GenServer.call(server, {:update, record, changes})
+  end
+
+  @doc "Creates a record with the attrs and puts it in the db"
+  def create(server, datum) do
+    GenServer.call(server, {:create, datum})
   end
 
   ## Server Callbacks
@@ -56,14 +64,31 @@ defmodule LandRegData do
 
   @impl true
   def handle_call({:get, attrs}, _from, data) do
-    {:reply, DB.Utils.get_by_attrs(data, attrs, @column_set), data}
+    if DB.Utils.attrs_exist_on_model?(attrs, __MODULE__) do
+      {:reply, DB.Utils.query_for_attrs(data, attrs), data}
+    else
+      {:error, "You are querying for an attribute that doesn't exist on the data! :("}
+    end
   end
 
   @impl true
-  def handle_cast({:update, record, changes}, data) do
-    with {:ok, updated_data} <- DB.Utils.update_record(record, changes, @column_set, data),
+  def handle_call({:update, record, changes}, _from, data) do
+    with {:ok, updated_record} <- DB.Utils.apply_changes(record, changes),
+         {:ok, updated_data} <- DB.Utils.update_record_in_data(record, updated_record, data),
          :ok <- write_to_file(updated_data) do
-      {:noreply, updated_data}
+      {:reply, updated_record, updated_data}
+    else
+      error -> IO.inspect(error)
+    end
+  end
+
+  @impl true
+  def handle_call({:create, datum}, _from, data) do
+    with {:ok, new_record} <- DB.Utils.apply_changes(%__MODULE__{id: UUID.uuid1()}, datum) do
+      updated_data = [new_record | data]
+      write_to_file(updated_data)
+
+      {:reply, new_record, updated_data}
     else
       error -> IO.inspect(error)
     end
@@ -77,15 +102,15 @@ defmodule LandRegData do
     |> Enum.map(&transform_to_struct/1)
   end
 
-  def transform_to_struct(data) when is_map(data) do
+  defp transform_to_struct(data) when is_map(data) do
     struct(__MODULE__, cast_data(data))
   end
 
-  def atomize_keys(map) when is_map(map) do
+  defp atomize_keys(map) when is_map(map) do
     for {k, v} <- map, do: {String.to_existing_atom(k), v}, into: %{}
   end
 
-  def write_to_file(data) do
+  defp write_to_file(data) do
     file = File.open!(@data_path, [:write, :utf8])
 
     data
@@ -97,13 +122,13 @@ defmodule LandRegData do
     :ok
   end
 
-  def cast_data(data) do
+  defp cast_data(data) do
     for {key, value} <- Map.drop(data, [:module]),
         do: {key, format_data(value, Map.fetch!(@columns, key))},
         into: %{}
   end
 
-  def format_data(datum, :number), do: String.to_integer(datum)
-  def format_data(datum, :date), do: Date.from_iso8601!(datum)
-  def format_data(datum, :string), do: datum
+  defp format_data(datum, :number), do: String.to_integer(datum)
+  defp format_data(datum, :date), do: Date.from_iso8601!(datum)
+  defp format_data(datum, :string), do: datum
 end
--- apps/ex_ample_backend/lib/utils.ex
+++ apps/ex_ample_backend/lib/utils.ex
@@ -1,39 +1,40 @@
 defmodule DB.Utils do
-  def update_record(record, changes, columns, data) do
-    changes_keys = List.foldr(changes, [], fn {key, _}, acc -> [key | acc] end) |> MapSet.new()
+  def update_record_in_data(record, updated_record, data) do
+    index =
+      Enum.with_index(data)
+      |> Enum.filter(fn {x, _} -> Map.equal?(x, record) end)
+      |> Enum.map(fn {_, i} -> i end)
+      |> hd()
 
-    if MapSet.subset?(changes_keys, columns) do
-      index =
-        Enum.with_index(data)
-        |> Enum.filter(fn {x, _} -> Map.equal?(x, record) end)
-        |> Enum.map(fn {_, i} -> i end)
-        |> hd()
+    {:ok, List.update_at(data, index, fn _ -> updated_record end)}
+  end
 
-      {:ok,
-       List.update_at(data, index, fn record -> Map.merge(record, Enum.into(changes, %{})) end)}
+  def apply_changes(old_record, changes) when is_list(changes) do
+    if attrs_exist_on_model?(changes, old_record.module) do
+      {:ok, Map.merge(old_record, Enum.into(changes, %{}))}
     else
-      {:error, "You are trying to change a column that doesn't exist on the data! :("}
+      {:error, "You are trying to change a column that doesn't exist! :("}
     end
   end
 
-  def get_by_attrs(data, attrs, columns) when is_list(attrs) do
-    attribute_keys = List.foldr(attrs, [], fn {key, _}, acc -> [key | acc] end) |> MapSet.new()
+  def attrs_exist_on_model?(attrs, model) when is_list(attrs) do
+    attr_keys = List.foldr(attrs, [], fn {key, _}, acc -> [key | acc] end) |> MapSet.new()
 
-    if MapSet.subset?(attribute_keys, columns) do
-      query_for_attrs(data, attrs)
+    if MapSet.subset?(attr_keys, model.column_set) do
+      true
     else
-      "You are querying for an attribute that doesn't exist on the data! :("
+      false
     end
   end
 
-  defp query_for_attrs(data, attrs) do
+  def query_for_attrs(data, attrs) do
     query_for_attrs(data, attrs, List.first(attrs))
   end
 
-  defp query_for_attrs(data, attrs, {attr_name, attr_value}) when length(attrs) > 0 do
+  def query_for_attrs(data, attrs, {attr_name, attr_value}) when length(attrs) > 0 do
     new_data = Enum.filter(data, fn datum -> Map.fetch!(datum, attr_name) == attr_value end)
     query_for_attrs(new_data, Enum.drop(attrs, 1), List.first(attrs))
   end
 
-  defp query_for_attrs(data, _, _), do: data
+  def query_for_attrs(data, _, _), do: data
 end
