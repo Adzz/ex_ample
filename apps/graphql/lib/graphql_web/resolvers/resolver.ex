2220c8d69da101889f0db42657abd75f37cf4dec
Adam Lancaster <lancaster28@hotmail.com>
2018-08-10 17:14:01 +0100

    adds a simple test for a simple query and mutation

--- apps/graphql/lib/graphql_web/resolvers/resolver.ex
+++ apps/graphql/lib/graphql_web/resolvers/resolver.ex
@@ -1,9 +1,12 @@
 defmodule Graphql.Resolver do
+<<<<<<< HEAD
   def addresses(args, _info) do
     # Check out the DB module to see the functions you can use
     # to interact with the database.
   end
 
+=======
+>>>>>>> adds a simple test for a simple query and mutation
   def smoke_test(_args, _info) do
     {:ok, "Yes!"}
   end
