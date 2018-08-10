faded3d45ea128bd7469c8b279973f9934bd17ea
Adam Lancaster <lancaster28@hotmail.com>
2018-08-10 20:11:43 +0100

    Adds failing test for first query

--- apps/graphql/lib/graphql_web/resovlers/resolver.ex
+++
@@ -1,9 +0,0 @@
-defmodule Graphql.Resolver do
-  def smoke_test(_args, _info) do
-    {:ok, "Yes!"}
-  end
-
-  def test_update(%{input: input}, _info) do
-    {:ok, input}
-  end
-end
--- apps/graphql/lib/graphql_web/schema.ex
+++ apps/graphql/lib/graphql_web/schema.ex
@@ -1,9 +1,17 @@
 defmodule Graphql.Schema do
   use Absinthe.Schema
 
+  object :address do
+    # which subfields do we need?
+  end
+
   query do
     field :is_this_thing_on, type: :string do
       resolve(&Graphql.Resolver.smoke_test/2)
+    end
+
+    field :get_addresses, type: list_of(:address) do
+      # add your args here, and the resolver
     end
   end
 
