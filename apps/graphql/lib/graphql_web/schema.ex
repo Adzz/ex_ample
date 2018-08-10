faded623b7238394eb2fb178c1207bb847e90e20
Adam Lancaster <lancaster28@hotmail.com>
2018-08-10 17:14:01 +0100

    adds a simple test for a simple query and mutation

--- apps/graphql/lib/graphql_web/schema.ex
+++ apps/graphql/lib/graphql_web/schema.ex
@@ -2,6 +2,15 @@ defmodule Graphql.Schema do
   use Absinthe.Schema
 
   query do
-    field(:hello, type: :string, resolve: fn _, _, _ -> {:ok, "It's alive"} end)
+    field :is_this_thing_on, type: :string do
+      resolve(&Graphql.Resolver.smoke_test/2)
+    end
+  end
+
+  mutation do
+    field :echo_text, type: :string do
+      arg(:input, :string)
+      resolve(&Graphql.Resolver.test_update/2)
+    end
   end
 end
