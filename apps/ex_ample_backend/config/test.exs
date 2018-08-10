f425516692d9d552569d029e3552c88c5808b633
Adam Lancaster <lancaster28@hotmail.com>
2018-08-10 20:37:15 +0100

    Adds failing test for first query

---
+++ apps/ex_ample_backend/config/test.exs
@@ -0,0 +1,5 @@
+use Mix.Config
+
+config :ex_ample_backend,
+  land_reg_source_path: "lib/data_sources/land_reg_data/test.csv",
+  address_source_path: "lib/data_sources/address/test.csv"
--- apps/graphql/test/integration/my_first_query_test.exs
+++ apps/graphql/test/integration/my_first_query_test.exs
@@ -1,9 +1,34 @@
 defmodule MyFirstQueryTest do
   use GraphqlWeb.ConnCase
+  require Graphql.TestHelpers
 
-  describe "queries for the field successfully" do
-    test "Queries for Average STC (in days) successfully" do
-      # seed database
+  # this is important to ensure a clean db between each test
+  setup do
+    DB.reset_db()
+    :ok
+  end
+
+  describe "queries for all of the addresses successfully" do
+    test "Queries for id, postcode and house number", %{conn: conn} do
+      {:ok, record} = DB.create(Address, %{house_number: 123, postcode: "N35 7ED"})
+
+      query = "{
+        getAddresses {
+          id
+          postcode
+          houseNumber
+        }
+      }"
+
+      res =
+        conn
+        |> Graphql.TestHelpers.make_graphql_query(query)
+
+      assert res["data"] == %{
+               "getAddresses" => [
+                 %{"houseNumber" => 123, "id" => record.id, "postcode" => "N35 7ED"}
+               ]
+             }
     end
   end
 end
