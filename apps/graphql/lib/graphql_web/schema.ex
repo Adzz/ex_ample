72afcadae247364b36a40e09758839ccf40a00b1
Adam Lancaster <lancaster28@hotmail.com>
2018-08-10 20:37:15 +0100

    Adds failing test for first query

--- apps/graphql/lib/graphql_web/schema.ex
+++ apps/graphql/lib/graphql_web/schema.ex
@@ -1,9 +1,17 @@
 defmodule Graphql.Schema do
   use Absinthe.Schema
 
+  object :address do
+    # which subfields do we need?
+  end
+
   query do
     field :is_this_thing_on, type: :string do
       resolve(&Graphql.Resolver.smoke_test/2)
+    end
+
+    field :get_addresses, type: list_of(:address) do
+      # We need to add a resolving function here!
     end
   end
 
