ed5533987edaacc6ef18a357612dc0eeab331baa
Adam Lancaster <lancaster28@hotmail.com>
2018-08-13 13:48:03 +0100

    Implements failing test for my second query

--- apps/graphql/lib/graphql_web/resolvers/resolver.ex
+++ apps/graphql/lib/graphql_web/resolvers/resolver.ex
@@ -3,7 +3,9 @@ defmodule Graphql.Resolver do
     DB.all(Address)
   end
 
-  # what new function shall we put here?
+  def land_reg_data(args, _info) do
+    # We probably need to head to the DB...
+  end
 
   def smoke_test(_args, _info) do
     {:ok, "Yes!"}
