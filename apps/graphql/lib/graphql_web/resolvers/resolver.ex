96c448e9f21cc7e9ad187f71987a7a6d939f2ed3
Adam Lancaster <lancaster28@hotmail.com>
2018-08-13 12:00:35 +0100

    implements my first query

--- apps/graphql/lib/graphql_web/resolvers/resolver.ex
+++ apps/graphql/lib/graphql_web/resolvers/resolver.ex
@@ -1,18 +1,8 @@
 defmodule Graphql.Resolver do
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> Adds failing test for first query
-  def addresses(args, _info) do
-    # Check out the DB module to see the functions you can use
-    # to interact with the database.
+  def addresses(_args, _info) do
+    DB.all(Address)
   end
 
-<<<<<<< HEAD
-=======
->>>>>>> adds a simple test for a simple query and mutation
-=======
->>>>>>> Adds failing test for first query
   def smoke_test(_args, _info) do
     {:ok, "Yes!"}
   end
