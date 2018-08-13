daa581915e9593d8f4483a820d9dd75a99945df1
Adam Lancaster <lancaster28@hotmail.com>
2018-08-13 13:25:35 +0100

    Implements failing test for my second query

--- apps/graphql/test/integration/my_second_query_test.exs
+++ apps/graphql/test/integration/my_second_query_test.exs
@@ -4,29 +4,32 @@ defmodule MySecondQueryTest do
 
   # this is important to ensure a clean db between each test
   setup do
-    DB.delete_all(LandRegData)
+    DB.reset_db()
     :ok
   end
 
-  describe "queries for the field successfully" do
-    test "Queries for average_time_to_sold (in days) successfully", %{conn: conn} do
-      {:ok, record} = DB.create(LandRegData, %{id: "10", average_time_to_sold: 6_000_000})
+  test "Queries for average_time_to_sold (in days) successfully", %{conn: conn} do
+    {:ok, address} = DB.create(Address, postcode: "N35 7ED", house_number: 123)
 
-      query = "query landRegQuery($id: ID!){
-        getLandRegData(id: $id) { averageTimeToSold }
+    {:ok, land_reg_data} =
+      DB.create(LandRegData, %{address_id: address.id, average_time_to_sold: 420})
+
+    query = "query landRegQuery($addressId: ID!){
+        landRegData(addressId: $addressId) { averageTimeToSold }
       }"
 
-      variables = """
-      {
-        "id": "#{record.id}"
-      }
-      """
+    variables = """
+    {
+      "addressId": "#{address.id}"
+    }
+    """
 
-      res =
-        conn
-        |> Graphql.TestHelpers.make_graphql_query(query, variables)
+    res =
+      conn
+      |> Graphql.TestHelpers.make_graphql_query(query, variables)
 
-      assert res["data"] == %{"getLandRegData" => %{"averageTimeToSold" => 6_000_000}}
-    end
+    assert res["data"] == %{
+             "landRegData" => [%{"averageTimeToSold" => land_reg_data.average_time_to_sold}]
+           }
   end
 end
