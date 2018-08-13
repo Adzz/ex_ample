e98ab80e29dd5e7d8490e1f5bdcf6ac63846c4cf
Adam Lancaster <lancaster28@hotmail.com>
2018-08-13 14:41:44 +0100

    implements my second query answers

--- apps/graphql/lib/graphql_web/resolvers/resolver.ex
+++ apps/graphql/lib/graphql_web/resolvers/resolver.ex
@@ -4,7 +4,11 @@ defmodule Graphql.Resolver do
   end
 
   def land_reg_data(args, _info) do
-    # We probably need to head to the DB...
+    DB.get(LandRegData, address_id: args.address_id)
+  end
+
+  def create_address(%{input: input}, _info) do
+    DB.create(Address, postcode: input.postcode, house_number: input.house_number)
   end
 
   def smoke_test(_args, _info) do
